19d844187685109ca65695fa9fa340d4b7e94fc9
n1cejkee91 <n1cejkee91@gmail.com>
2021-02-11 21:40:54 +0300

    Add model "Vacancy"

--- scraping_service/scraping/admin.py
+++ scraping_service/scraping/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import City, Professions
-
+from .models import City, Professions, Vacancy
 
 admin.site.register(City)
 admin.site.register(Professions)
+admin.site.register(Vacancy)
--- scraping_service/scraping/models.py
+++ scraping_service/scraping/models.py
@@ -24,3 +24,20 @@ class Professions(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name='Название вакансии')
+    company = models.CharField(max_length=250, verbose_name='Компания')
+    description = models.TextField(verbose_name='Описание вакансии')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город')
+    profession = models.ForeignKey('Professions', on_delete=models.CASCADE, verbose_name='Должность')
+    timestamp = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
+
+    def __str__(self):
+        return self.title
