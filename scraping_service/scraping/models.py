1d61a365753e8e5c8198b05eb60fa778a20449cd
n1cejkee91 <n1cejkee91@gmail.com>
2021-02-11 21:03:36 +0300

    Add model "Professions"

--- scraping_service/scraping/models.py
+++ scraping_service/scraping/models.py
@@ -1,13 +1,26 @@
 from django.db import models
+from autoslug import AutoSlugField
 
 
 class City(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Населенный пункт')
-    slug = models.SlugField(max_length=50, unique=True, blank=True)
+    name = models.CharField(max_length=50, verbose_name='Населенный пункт', unique=True)
+    slug = AutoSlugField(populate_from='name', unique=True)
 
     class Meta:
         verbose_name = 'Населенный пункт'
         verbose_name_plural = 'Населенные пункты'
+
+    def __str__(self):
+        return self.name
+
+
+class Professions(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Профессия или должность', unique=True)
+    slug = AutoSlugField(populate_from='name', unique=True)
+
+    class Meta:
+        verbose_name = 'Профессия или должность'
+        verbose_name_plural = 'Профессии или должности'
 
     def __str__(self):
         return self.name
