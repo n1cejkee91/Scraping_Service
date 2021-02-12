be45d0c3a8f3da88bf8360f1cafe4504b888d9f9
n1cejkee91 <n1cejkee91@gmail.com>
2021-02-12 20:56:31 +0300

    Add: - base.html - home.html - test.html Change views.py and urls.py

--- scraping_service/scraping/views.py
+++ scraping_service/scraping/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Vacancy
 
-# Create your views here.
+
+def home_view(request):
+    vacancies = Vacancy.objects.all()
+    return render(request, 'scraping/home.html', {'vacancies': vacancies})
+
+
+def test(request):
+    return render(request, 'scraping/test.html')
