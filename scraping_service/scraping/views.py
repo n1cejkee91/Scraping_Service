e256d7e20eb73cc2f016a214a0c2e35e8c3c2008
n1cejkee91 <n1cejkee91@gmail.com>
2021-02-20 19:12:13 +0300

    Modify view.py, list.html

--- scraping_service/scraping/views.py
+++ scraping_service/scraping/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Vacancy
 from .forms import FindForm
@@ -13,7 +14,7 @@ def list_view(request):
     form = FindForm(request.GET)
     city = request.GET.get('city')
     language = request.GET.get('language')
-    vacancies = []
+    context = {'city': city, 'language': language, 'form': form}
     if city or language:
         _filter = {}
         if city:
@@ -21,7 +22,11 @@ def list_view(request):
         if language:
             _filter['language__slug'] = language
         vacancies = Vacancy.objects.filter(**_filter)
-    return render(request, 'scraping/list.html', {'vacancies': vacancies, 'form': form})
+        paginator = Paginator(vacancies, 10)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['vacancies'] = page_obj
+    return render(request, 'scraping/list.html', context)
 
 
 def test(request):
