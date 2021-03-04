6c4a02833f261858699cd3891ccd5b7ec4cf0c8a
n1cejkee91 <n1cejkee91@gmail.com>
2021-03-04 19:56:01 +0300

    Create view, form and template to update and delete User

--- scraping_service/accounts/forms.py
+++ scraping_service/accounts/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.hashers import check_password
 
+from scraping.models import City, Languages
+
 User = get_user_model()
 
 
@@ -39,3 +41,16 @@ class UserRegistrationForm(forms.ModelForm):
         if data['password'] != data['password2']:
             raise forms.ValidationError('Пароли не совпадают')
         return data['password2']
+
+
+class UserUpdateForm(forms.Form):
+    city = forms.ModelChoiceField(queryset=City.objects.all(), to_field_name='slug', required=True,
+                                  widget=forms.Select(attrs={'class': 'form-control'}), label='Город')
+    language = forms.ModelChoiceField(queryset=Languages.objects.all(), to_field_name='slug', required=True,
+                                      widget=forms.Select(attrs={'class': 'form-control'}),
+                                      label='Язык программирования')
+    send_email = forms.BooleanField(required=False, widget=forms.CheckboxInput, label='Получать рассылку?')
+
+    class Meta:
+        model = User
+        fields = ('city', 'language', 'send_email')
--- scraping_service/accounts/views.py
+++ scraping_service/accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 
-from accounts.forms import UserLoginForm, UserRegistrationForm
+from accounts.forms import UserLoginForm, UserRegistrationForm, UserUpdateForm
 
+User = get_user_model()
 
 def login_view(request):
     form = UserLoginForm(request.POST or None)
@@ -28,3 +29,31 @@ def register_view(request):
         new_user.save()
         return render(request, 'accounts/register_done.html', {'new_user': new_user})
     return render(request, 'accounts/register.html', {'form': form})
+
+
+def update_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            form = UserUpdateForm(request.POST)
+            if form.is_valid():
+                user.city = form.cleaned_data['city']
+                user.language = form.cleaned_data['language']
+                user.send_email = form.cleaned_data['send_email']
+                user.save()
+                return redirect('accounts:update')
+        else:
+            form = UserUpdateForm(initial={'city': user.city, 'language': user.language, 'send_email': user.send_email})
+            return render(request, 'accounts/update.html', {'form': form})
+
+    else:
+        return redirect('accounts:login')
+
+
+def delete_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            user = User.objects.get(pk=user.pk)
+            user.delete()
+    return redirect('home')
