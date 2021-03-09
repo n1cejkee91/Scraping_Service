6044e3e98ae70617c1f74411a1866f4e303d285f
n1cejkee91 <n1cejkee91@gmail.com>
2021-03-09 22:37:05 +0300

    Add send_emails.py and change settings.py

---
+++ scraping_service/send_emails.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+import django
+from django.contrib.auth import get_user_model
+from django.core.mail import EmailMultiAlternatives
+
+proj = os.path.dirname(os.path.abspath('manage.py'))
+sys.path.append(proj)
+os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
+
+django.setup()
+from scraping.models import Vacancy
+
+User = get_user_model()
+qs = User.objects.filter(send_email=True).values('city', 'language', 'email')
+users_dct = {}
+for i in qs:
+    users_dct.setdefault((i['city'], i['language']), [])
+    users_dct[(i['city'], i['language'])].append(i['email'])  # Ключ это пара id (город и ЯП), значение email'ов
+if users_dct:
+    params = {'city_id__in': [], 'language_id__in': []}
+    for pair in users_dct.keys():
+        params['city_id__in'].append(pair[0])
+        params['language_id__in'].append(pair[1])  # Значения по городам и ЯП, которые необходимо получить из БД
+    qs = Vacancy.objects.filter(**params).values()
+    vacancies = {}
+    for i in qs:
+        vacancies.setdefault((i['city_id'], i['language_id']), [])
+        vacancies[(i['city_id'], i['language_id'])].append(i)
+    for keys, emails in users_dct.items():
+        rows = vacancies.get(keys, [])
+        html = ''
+        for row in rows:
+            html += f'<h2><a href="{ row["url"] }">{ row["title"] }</a></h2>'
+            html += f'<p>{ row["description"] }</p>'
+            html += f'<p>{row["company"]}</p><br><hr>'
+
+
+
+
+subject, from_email, to = 'hello', 'from@example.com', 'to@example.com'
+text_content = 'This is an important message.'
+html_content = '<p>This is an <strong>important</strong> message.</p>'
+msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+msg.attach_alternative(html_content, "text/html")
+msg.send()
