9312eb15ee2a22279e5e2af94f4f28f692e6c1d5
n1cejkee91 <n1cejkee91@gmail.com>
2021-03-18 21:35:52 +0300

    Update production_settings.py

--- scraping_service/scraping_service/settings/production_settings.py
+++ scraping_service/scraping_service/settings/production_settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["vacansy-scraping-service.herokuapp.com"]
+ALLOWED_HOSTS = ["vacancy-scraping.herokuapp.com/"]
 
 # Application definition
 
