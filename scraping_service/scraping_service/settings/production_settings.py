29f7ff587813f0c07b4547ca6e16a2950406eac0
n1cejkee91 <n1cejkee91@gmail.com>
2021-03-19 19:10:45 +0300

    update production_settings.py

--- scraping_service/scraping_service/settings/production_settings.py
+++ scraping_service/scraping_service/settings/production_settings.py
@@ -124,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'us-US'
+LANGUAGE_CODE = 'ru-Ru'
 
 TIME_ZONE = 'UTC'
 
