afba2a01cb6ddf379ce4d60deef349e7367c04b4
Ivan <n1cejkee91@gmail.com>
2021-03-23 11:27:51 +0300

    Update admin.py, views.py in accounts

--- scraping_service/accounts/admin.py
+++ scraping_service/accounts/admin.py
@@ -5,7 +5,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.core.exceptions import ValidationError
 
-from scraping_service.accounts.models import MyUser
+from accounts.models import MyUser
 
 
 class UserCreationForm(forms.ModelForm):
