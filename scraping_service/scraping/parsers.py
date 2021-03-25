0920f2cdc390e70af84440f053e7f19cf1edff51
n1cejkee91 <n1cejkee91@gmail.com>
2021-03-25 20:29:52 +0300

    Update parsers.py

--- scraping_service/scraping/parsers.py
+++ scraping_service/scraping/parsers.py
@@ -1,5 +1,4 @@
-'''Парсер для сайтов'''
-import time
+"""Парсер для сайтов"""
 
 import requests
 from bs4 import BeautifulSoup as BS
@@ -150,7 +149,6 @@ def djinni(url, city=None, language=None):
 def msk_rabotaru(url, city=None, language=None):
     jobs = []
     errors = []
-    time.sleep(0.01)
     domain = 'https://www.rabota.ru'
     if url:
         resp = requests.get(url, headers=headers[randint(0, 5)])
@@ -181,7 +179,6 @@ def msk_rabotaru(url, city=None, language=None):
 def spb_rabotaru(url, city=None, language=None):
     jobs = []
     errors = []
-    time.sleep(0.01)
     domain = 'https://spb.rabota.ru/'
     if url:
         resp = requests.get(url, headers=headers[randint(0, 5)])
